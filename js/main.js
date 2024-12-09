397e809fbe2f54dfb51dc44209c728bb169df15e
Aidil Novelin F <163558347+AidilNvfawd@users.noreply.github.com>
2024-12-10 04:04:44 +0700

    Menambahkan Assets

--- js/main.js
+++ js/main.js
@@ -13,36 +13,3 @@ const showMenu = (toggleId, navId) => {
 }
 
 showMenu('nav-toggle', 'nav-menu');
-
-// Mendapatkan referensi tombol dan kontainer PDF
-const showPDFBtn = document.getElementById("showPDFBtn");
-const pdfContainer = document.getElementById("pdf-container");
-const closePDFBtn = document.getElementById("closePDFBtn");
-
-// Menambahkan event listener untuk tombol tampilkan PDF
-showPDFBtn.addEventListener("click", function() {
-    pdfContainer.style.display = "flex"; // Menampilkan PDF
-});
-
-// Menambahkan event listener untuk tombol tutup PDF
-closePDFBtn.addEventListener("click", function() {
-    pdfContainer.style.display = "none"; // Menyembunyikan PDF
-});
-
-// Menutup PDF saat mengklik area di luar modal
-pdfContainer.addEventListener("click", function(event) {
-    if (event.target === pdfContainer) {
-        pdfContainer.style.display = "none";
-    }
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-    const navLinks = document.querySelectorAll(".nav-link a");
-
-    navLinks.forEach(link => {
-        link.addEventListener("click", function() {
-            navLinks.forEach(nav => nav.classList.remove("active"));
-            this.classList.add("active");
-        });
-    });
-});
