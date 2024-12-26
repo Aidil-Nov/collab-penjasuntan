d4e444aa5a149131f43ecd09123a6358324aadf5
Aidil Novelin F <163558347+AidilNvfawd@users.noreply.github.com>
2024-12-27 00:33:12 +0700

    Akreditasi Diperbaiki

--- akreditasi.php
+++ akreditasi.php
@@ -1,81 +1,65 @@
-<?php include('./include/header.php');?>
+<?php include './include/header.php';?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Akreditasi</title>
+    <title>Akademik | Akreditasi</title>
     <style>
-        /* Akreditasi */
-    .card-grid {
+        /* BAGIAN AKREDITASI IMAGE */
+        .card-grid {
         display: grid;
         grid-template-columns: 1fr;
-    }
+        }
 
-    .card-image {
+        .card-image {
         margin-top: 1rem;
         padding: 10px;
-    }
-    
-    .card-image .img{
-        transform: scale(1);
-        transition: 0.5s ease-in-out;
-        cursor: pointer;
-    }
+        width: 100%;
+        box-shadow: var(--shadow);
+        }
 
-    .card-image img {
-        box-shadow:var(--shadow);
-    }
-
-    .card-image .img:hover {
-        transform: scale(1.05);
-    }
-
-    .card-image h3 {
+        .card-image h3 {
         text-align: center;
         font-weight: 300;
-    }
+        }
 
-    .card-image img {
+        .card-image img {
         display: flex;
         justify-content: center;
         margin: auto;
         width: 80%;
         height: auto;
         object-fit: cover;
-    }
+        }
     </style>
 </head>
-
 <body>
+    <!-- Bagian Akreditasi -->
     <section class="akreditasi-section section-container">
-    <h1 class="section-subheader">Akreditasi</h1>
-    <div class="card-grid">
-        <div class="card-image">
-            <div class="img"><img src="./assets/Akreditasi B 2020-2025.png" alt="Akreditasi B 2020-2025">
+        <h1 class="section-subheader">Akreditasi</h1>
+        <div class="card-grid">
+            <div class="card-image">
+                <img src="./resource/Akreditasi B 2020-2025.png" alt="">
                 <h3>
                     Akreditsi Tahun 2020 hingga 2025
                 </h3>
             </div>
-        </div>
-        <div class="card-image">
-            <div class="img">
-                <img src="./assets/Akreditasi B 2015-2020.png" alt="Akreditasi B 2015-2020">
+            <div class="card-image">
+                <img src="./resource/Akreditasi B 2015-2020.png" alt="">
                 <h3>
                     Akreditsi Tahun 2015 hingga 2020
                 </h3>   
             </div>
-        </div>
-        <div class="card-image">
-            <div class="img">
-                <img src="./assets/AKREDITASI C TAHUN 2011-2016.png" alt="Akreditasi C TAHUN 2011-2016">
-                <h3>
-                    Akreditsi Tahun 2011 hingga 2016
-                </h3>
+            <div class="card-image">
+                <img src="./resource/AKREDITASI C TAHUN 2011-2016.png" alt="">
+                    <h3>
+                        Akreditsi Tahun 2011 hingga 2016
+                    </h3>
             </div>
-        
         </div>
-    </div>
     </section>
 </body>
 </html>
+
+
 <?php include('./include/footer.php') ?>
